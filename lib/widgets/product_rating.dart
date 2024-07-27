50ebaf4731adedd368670c66d5f94b3eec627491
Abhijit Maiti <avhijitmaiti0123@gmail.com>
2024-07-28 03:20:48 +0530

    product ratings view #26

---
+++ lib/widgets/product_rating.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:telo/constants.dart';
+
+class ProductRating extends StatelessWidget {
+  const ProductRating({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Ratings',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        SizedBox(height: 18),
+        Row(
+
+          children: [
+            Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+              Text("4.5",
+                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold)),
+              SizedBox(width: 8),
+              Icon(Icons.star, size: 34, color: Colors.amber)
+            ])),
+            Expanded(
+                child: Column(
+              children: [
+                _ratingProgress("5", 5.0, Colors.green, 10),
+                _ratingProgress("4", 5.0, Colors.lightGreenAccent, 10),
+                _ratingProgress("5", 5.0, Colors.lightGreen, 10),
+                _ratingProgress("2", 5.0, Colors.yellow, 10),
+                _ratingProgress("1", 5.0, Colors.red, 10),
+                Divider(),
+                Text("total ratings")
+              ],
+            ))
+          ],
+        )
+      ],
+    );
+  }
+
+  _ratingProgress(title, progress, color, count) {
+    return Column(
+      children: [
+        Text(
+          title + ' ',
+          style: TextStyle(fontSize: 12, height: 1.8),
+        ),
+        Icon(
+          Icons.star,
+          size: 12,
+        ),
+        SizedBox(
+          width: 8,
+        ),
+        Expanded(
+            child: LinearProgressIndicator(
+                value: progress,
+                valueColor: AlwaysStoppedAnimation<Color>(color))),
+        SizedBox(
+          width: 8,
+        ),
+        Text(
+          count.toString(),
+          style: TextStyle(fontSize: 12, height: 1.8),
+        ),
+      ],
+    );
+  }
+}

0c0db78db73280aeb46fb44372bdaff2bfd1f320
Abhijit Maiti <avhijitmaiti0123@gmail.com>
2024-07-28 14:17:12 +0530

    develop option in product #24

--- lib/product_details/product_details_screen.data.dart
+++ lib/product_details/product_details_screen.data.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:telo/product_details/product_details_cubit.dart';
 import 'package:telo/product_details/product_details_state.dart';
+import 'package:telo/widgets/product_rating.dart';
 
+import '../models/product_detail_model.dart';
 import '../widgets/product_image_carousel.dart';
 
 class ProductDetailScreen extends StatefulWidget {
@@ -17,7 +19,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
-  List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+  // List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 
   @override
   void initState() {
@@ -31,21 +33,36 @@ class _ProductDetailScreenState extends State<ProductDetailScreen>
     super.dispose();
   }
 
-  String dropdownValue = 'One';
+  late String dropdownValue = ' ' ;
+  Options? selectedOptions;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Product details"),
-        ),
+
         body: BlocConsumer<ProductDetailsCubit, ProductDetailsState>(
             builder: (context, state) {
               if (state is ProductDetailsLoaded) {
+                if(dropdownValue.isEmpty){
+                  dropdownValue = state.model.options![0].id!;
+                }
                 return SingleChildScrollView(
+
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                       AppBar(
+                        title: Text("Product details"),
+                         foregroundColor: Colors.black,
+                         backgroundColor: Colors.transparent,
+                           elevation: 0,
+                           actions:
+                            [
+                           IconButton(onPressed: () {}, icon: Icon(Icons.favorite_outline)),
+                           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+                           ],
+
+                      ),
                       ProductImageCarousel(state.model.options![0].images!
                           .map((image) => image.image!)
                           .toList()),
@@ -71,7 +88,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen>
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-
                             Text(
                               "Rs." + state.model.price!.toString(),
                               style: TextStyle(
@@ -86,7 +102,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen>
                               isExpanded: true,
                               value: dropdownValue,
                               icon: const Icon(Icons.keyboard_arrow_down),
-
                               style: const TextStyle(color: Colors.blue),
                               underline: Container(
                                 height: 2,
@@ -98,32 +113,26 @@ class _ProductDetailScreenState extends State<ProductDetailScreen>
                                   dropdownValue = value!;
                                 });
                               },
-                              items: <String>[
-                                'One',
-                                'Two',
-                                'Three',
-                                'Four'
-                              ].map<DropdownMenuItem<String>>((String value) {
+                              items: state.model.options!
+                                  .map<DropdownMenuItem<String>>((Options option) {
                                 return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
+                                  value: option.id,
+                                  child: Text(option.option!),
                                 );
                               }).toList(),
                             ),
-                            SizedBox(
-                              height: 16,
-                            ),
+                            SizedBox(height: 16),
                             Text(
                               "Description",
                               style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                                  fontSize: 18, fontWeight: FontWeight.bold),
                             ),
-                            SizedBox(
-                              height: 8,
-                            ),
+                            SizedBox(height: 8),
                             Text(state.model.description!),
+                            SizedBox(height: 16),
+
+                            ProductRating(),
+
                           ],
                         ),
                       ),
